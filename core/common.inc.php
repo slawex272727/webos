2f07f8d39a35e59949de2cbbf86d43694eb863f9
Piotr Merski <merskip@gmail.com>
2013-04-05 20:52:44 +0200

    Rzucanie wyjątkami oraz plik common.inc.php
     * Automatyczne wczytywanie klas
     * Przy błedzie PHP zostanie wyrzucony wyjątek

---
+++ core/common.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Przerabia błedy php na wyjątki
+ */
+function exception_error_handler($errno, $errstr, $errfile, $errline)
+{
+	throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
+
+/**
+ * Automatycznie importuje potrzebną klasę
+ */
+function __autoload($className)
+{
+    $pathList = array(
+    		    "core/{$class}.php",
+    		    "core/{$class}.class.php"
+    		);
+    
+    foreach ($pathList as $path)
+    {
+    	if(file_exists($path))
+    	{
+    		require_once $path;    
+    		return true;
+    	}
+    }
+    
+    throw new Exception("Class {$class} was not found!");
+}
+
+?>
