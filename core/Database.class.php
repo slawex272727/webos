fa74e44484681429792c4b58d3d3a048c016c441
Piotr Merski <merskip@gmail.com>
2013-04-05 20:47:50 +0200

    Początek klasy Database

---
+++ core/Database.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Tworzy poziom abstrakcji do obługi bazy danych.
+ */
+class Database
+{
+    public function __construct($host = null, $user = null, $password = null, $database = null)
+    {
+        $this->mysqli = new mysqli($host, $user, $password, $database);
+        
+        if ($this->mysqli->connect_error)
+        	throw new Exception($this->mysqli->connect_error);
+    }
+    
+    public function __destruct()
+    {
+        $mysqli->close();
+    }
+    
+    private $mysqli = null;
+}
+
+?>
