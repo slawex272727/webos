e7ebc25b656e0bbd1e89c3907c7c0710f5408bb3
slawex272727 <tojaja@interia.eu>
2013-04-05 20:18:02 +0300

    Create head.php

---
+++ core/head.php
@@ -0,0 +1,14 @@
+<?php
+
+function run_headers()
+  {
+		echo'<!DOCTYPE html>'.
+			'<html>'.
+			'<head>'.
+			'<meta charset="utf-8"/>'.
+			'<title>nazwa.pl</title>'.
+			'<link rel="icon" href="images/favicon.ico" type="image/x-icon"/>'.
+			'<link rel="stylesheet" href="css/style.css" type="text/css"/>'.
+			'</head>';
+	}
+?>
