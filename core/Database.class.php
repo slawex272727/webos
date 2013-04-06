b5bbcbcc5517c38d0b806625edeb61c6eef59e9e
Piotr Merski <merskip@gmail.com>
2013-04-06 15:19:40 +0200

    Dodanie Klasy DatabaseQuerySelect, DatabaseQuery oraz aliasów do nich
     * Poprawka do zamykania połaczenia z bazą danych
     * Metoda Database::select
     * Poprawka do błędów w Database::query
     * Nowa klasa DatabaseQuerySelect
     ^ * Określanie kolumn z użyciem aliasów
         * Określanie tabeli z użyciem alisu
     * Nowa klasa DatabaseQuery
     ^ * Wskaźnik na bazę danych, jeśli jest można wywołać metodę exec

--- core/Database.class.php
+++ core/Database.class.php
@@ -21,7 +21,12 @@ class Database
     
     public function __destruct()
     {
-        $mysqli->close();
+        $this->mysqli->close();
+    }
+    
+    public function select($table, $alias = null)
+    {
+        return new DatabaseQuerySelect($table, $alias, $this);
     }
     
     /**
@@ -34,11 +39,10 @@ class Database
     {
         $result = $this->mysqli->query($query);
         
-        if ($this->mysqli->connect_error)
-        	throw new Exception($this->mysqli->connect_error);
+        if ($this->mysqli->errno)
+        	throw new Exception($this->mysqli->error);
         
         $table = array();
-        
         while ($row = $result->fetch_object())
             $table[] = $row;
         
@@ -47,6 +51,96 @@ class Database
     }
     
     private $mysqli = null;
+}
+
+class DatabaseQuerySelect extends DatabaseQuery
+{
+    public function __construct($table, $alias = null, Database $database = null)
+    {
+        parent::__construct("SELECT", $database);
+        
+        $this->table = $table;
+        $this->alias = $alias;
+    }
+    
+    public function fields(array $fileds)
+    {
+        $this->fields = $fileds;
+        
+        return $this;
+    }
+    
+    public function toString()
+    {
+        return "{$this->instruction} {$this->toStringFields()} FROM {$this->toStringTable()}";
+    }
+    
+    private function toStringFields()
+    {
+        if ($this->fields === null)
+            return "*";
+        
+        $str = "";
+        
+        foreach ($this->fields as $key => $value)
+        {
+            if (is_numeric($key) == false)
+                $str .= "{$key} as {$value}";
+            else
+                $str .= $value;
+            
+            if ($value !== end($this->fields))
+                $str .= ", ";
+        }
+        
+        return $str;
+    }
+    
+    private function toStringTable()
+    {
+        if ($this->alias !== null)
+            return "{$this->table} as {$this->alias}";
+        else
+            return $this->table;
+    }
+    
+    private $table = null;
+    private $alias = null;
+    
+    private $fields = null; 
+}
+
+class DatabaseQuery
+{
+    public function __construct($instruction, Database $database = null)
+    {
+        $this->instruction = $instruction;
+        $this->database = $database;
+    }
+    
+    /**
+     * Wykonuje polecenie SQL
+     * return: Odpowiedź bazy danych
+     * throw: Wskaźnik na bazę danych jest pusty
+     */
+    public function exec()
+    {
+        if ($this->database === null)
+            throw new Exception("Pointer to an object database is empty");
+        
+        return $this->database->query($this->toString());
+    }
+    
+    /**
+     * return: Wygenerowane zapytanie SQL
+     */
+    public function toString()
+    {
+        return $this->instruction;
+    }
+    
+    protected $instruction = null;
+    protected $database = null;
 }
 
 ?>
