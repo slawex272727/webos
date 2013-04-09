e5b148846f9a02035478ddfaa6650c0e042ecdfb
Slawek Wojcik <tojaja@interia.eu>
2013-04-09 21:16:46 +0200

    Logowanko....

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<html>
 <?php
-echo "rejestracja i logowania";
+
+try
+{
+    require_once "core/head.php";
+    require_once "core/generaction_window.php";
+    
+    $content = "";
+    
+    $generatorFieldsIcons = new GeneratorFieldsIcons;
+    
+    $content .= getHtmlHeader().
+                    "<body>".
+					"<div id='start_window'>".
+						"<div id='top_start_menu'>".
+						
+						"</div>".
+					"</div>".
+                    "</body>".
+                "</html>";
+    
+    print $content;
+}
+catch (Exception $e)
+{
+   print "FATAL ERROR: {$e->getMessage()}";
+}
+
 ?>
